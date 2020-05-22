5ea90f55a35ab823640513baf291e1b1ef5e3999
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-05-21 22:13:55 -0300

    chore:Estado e Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+
+  // useState retorna um array com 2 posições:
+  //
+  // 1. Variável com o seu valor inicial
+  // 2. Função para atualizarmos esse valor
+  //
+
+  const [projects , setProjects] = useState([ 
+    'Desenvolvimento de Backend',
+    'Desenvolvimento de Frontend',
+    'Desenvolvimento de Mobile'
+  ]);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Home">
-        <lu>
-          <li>Projects</li>
-          <li>About</li>
-        </lu>
-      </Header>
+      <Header title="Home" />
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default function Reader({ title, children }) {
+export default function Reader({ title }) {
   return (
     <header>
       <h1>{title}</h1>
-      {children}
     </header>
   );
 }
