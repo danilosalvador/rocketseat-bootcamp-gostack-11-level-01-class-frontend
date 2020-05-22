6139ebb1d1b37a0382bb01c8437e4b0f7c6a1203
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-05-21 21:51:20 -0300

    chore:Componentização e Propriedades

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header title="Home">
+        <lu>
+          <li>Projects</li>
+          <li>About</li>
+        </lu>
+      </Header>
+    </>
+  );
+}
+
+export default App;
