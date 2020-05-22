4dd71c42051f34a9ddacaf1c25710e5022303f18
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-05-21 23:17:36 -0300

    features:Listando Projetos da API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
+import api from './services/api';
 
 function App() {
 
@@ -13,11 +14,23 @@ function App() {
   // 2. Função para atualizarmos esse valor
   //
 
-  const [projects , setProjects] = useState([ 
-    'Desenvolvimento de Backend',
-    'Desenvolvimento de Frontend',
-    'Desenvolvimento de Mobile'
-  ]);
+  const [projects , setProjects] = useState([]);
+
+  // useEffect
+  //
+  // Disparar funções sempre que for alterada (ou não)
+  // Recebe dois parâmetros:
+  //
+  // 1. Qual função para disparar
+  // 2. Qual váriavel deve ser monitorada qdo alterada para ser disparada
+  //    Se for vazia, irá ser dispara apenas uma vez.
+  //
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `Novo Projeto ${Date.now()}`]);
@@ -29,7 +42,7 @@ function App() {
       <Header title="Home" />
       <img width={300} src={backgroundImage}/>
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
